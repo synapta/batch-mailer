f162393b4c09421d3b014a96bc257683a4b3cccf
Giuseppe Futia <giuseppe.futia@synapta.it>
2021-03-25 16:31:37 +0100

    First version of pyinstaller scripts to generate windows exe

--- pyinstaller/hook-cefpython3.py
+++ pyinstaller/hook-cefpython3.py
@@ -217,6 +217,23 @@ hiddenimports = [
     "types",
     "urllib",
     "weakref",
+    'uvicorn.lifespan.off',
+    'uvicorn.lifespan.on',
+    'uvicorn.lifespan',
+    'uvicorn.protocols.websockets.auto',
+    'uvicorn.protocols.websockets.wsproto_impl',
+    'uvicorn.protocols.websockets_impl',
+    'uvicorn.protocols.http.auto',
+    'uvicorn.protocols.http.h11_impl',
+    'uvicorn.protocols.http.httptools_impl',
+    'uvicorn.protocols.websockets',
+    'uvicorn.protocols.http',
+    'uvicorn.protocols',
+    'uvicorn.loops.auto',
+    'uvicorn.loops.asyncio',
+    'uvicorn.loops.uvloop',
+    'uvicorn.loops',
+    'uvicorn.logging'
 ]
 if is_py2:
     hiddenimports += [
